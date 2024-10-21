c209da18a76bba4d77102fdf2da096868cfec6f1
Yomna Ibrahim <yomnai104@gmail.com>
2024-10-21 19:17:31 +0200

    + settings localization + shared preference

--- lib/providers/settingsProvider.dart
+++ lib/providers/settingsProvider.dart
@@ -1,27 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-//observable , Publisher
-class SettingsProvider extends ChangeNotifier{
-  ThemeMode themeMode = ThemeMode.light;
+class SettingsProvider extends ChangeNotifier {
+  ThemeMode themeMode;
 
-  changeTheme(ThemeMode newMode){
-    if(themeMode == newMode)
-      return;
+  SettingsProvider({required String themeMode})
+      : themeMode = themeMode == "dark" ? ThemeMode.dark : ThemeMode.light;
+
+  changeTheme(ThemeMode newMode) async {
+    if (themeMode == newMode) return;
+
     themeMode = newMode;
+
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString("themeMode", newMode == ThemeMode.dark ? "dark" : "light");
+
     notifyListeners();
   }
 
   String language = "en";
 
-  changeLanguage(String newLang){
-    if (newLang == language)
-      return;
+  changeLanguage(String newLang) {
+    if (newLang == language) return;
     language = newLang;
     notifyListeners();
-
   }
-
-
-
-
 }
